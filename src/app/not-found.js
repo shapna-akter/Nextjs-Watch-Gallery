cc3130a8ddb524fa82b99af8d03dc05cb3d57bfb
shapna28 <shapna088@gmail.com>
2023-07-28 19:33:36 +0600

    first commit

---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+    return (
+        <div className='text-6xl flex justify-center items-center h-[100vh]'>
+            <h1>404!!! Page not found</h1>
+        </div>
+    );
+};
+
+export default NotFoundPage;
---
+++ src/app/product/[id]/loading.js
@@ -0,0 +1,9 @@
+const loading = () => {
+  return (
+    <div className="text-6xl flex justify-center items-center h-[100vh]">
+      <span className="loading loading-bars loading-lg"></span>
+    </div>
+  );
+};
+
+export default loading;
